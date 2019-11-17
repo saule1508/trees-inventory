31c67377356269bd06207f0d2fdc61d4947616b4
Pierre Timmermans <ptim007@yahoo.com>
2019-11-17 15:31:25 +0100

    default language

--- i18n.js
+++ i18n.js
@@ -2,12 +2,17 @@ const NextI18Next = require('next-i18next').default
 
 
 module.exports = new NextI18Next({
-  defaultLanguage: 'en',
-  otherLanguages: ['nl','fr'],
+  defaultLanguage: 'fr',
+  otherLanguages: ['nl','en'],
   defaultNS: 'common',
   localeSubpaths: {
     fr: 'fr',
     nl: 'nl',
     en: 'en',
+  },
+  detection: {
+    lookupCookie: 'next-i18next',
+    order: ['cookie', 'querystring', 'localStorage', 'path', 'subdomain'],
+    caches: ['cookie'],
   },
 })
--- pages/about.js
+++ pages/about.js
@@ -27,6 +27,45 @@ const AboutEn = () => {
   );
 };
 
+const AboutFr = () => {
+  return (
+    <div>
+      <p>
+        La carte des arbres remarquables est basée sur les données <i>Opendata</i> publiées par la région de Bruxelles. 
+        Voir sur leur site ou ils expliquent le concept de Smart City :
+        <br/>
+        <a href="http://opendatastore.brussels/en/">http://opendatastore.brussels/fr/</a>
+      </p>
+      <p>
+        L'inventaire des arbres remarquables est consultable sur le site <a href="https://arbres-inventaire.irisnet.be/">arbres-inventaire.irisnet.be</a>, 
+        mais pour l'instant il n'y a pas de carte et je ne pense pas que ça va venir.. Il y a bien une carte disponible sur urbis online, mais son utilisation 
+        est plutôt difficile et je pense que ça s'addresse aux pro plus qu'aun grand piblic.
+        <br/>
+        <a href="https://urbisonline.brussels/">urbisonline.brussels</a>
+      </p>
+      <p>
+        Il y a un site que j'aime beaucoup, <a href="www.woodwideweb.be">www.woodwideweb.be</a>, qui m'a donné l'idée d'afficher une carte dynamiquement 
+        et l'envie d'apprendre à le faire.
+        Cependant sur woodwideweb tout les arbres de l'inventaire ne sont pas repris 
+        (les espèces moins communes ne sont pas chargées dans leur base de données) et le filtrage sur une espèce particulière n'est pas possible.
+      </p>
+      <p>
+        L'intérêt principal de ce site est d'aider à reconnaître les arbres, en indiquant ou aller pour trouver une espèce particulière par exemple.
+        L'idéal serait d'avoir les arbres d'alignement, ces arbres sont normalement pourvu d'un numéro d'inventaire mais il est actuellement difficile de 
+        trouver l'information correspondante à ce numéro sur internel (c'est possible quoique difficile sur un des sites de urbisonline, il faut trouver 
+        la bonne couche à ajouter à la carte).
+      </p>
+    </div>
+  );
+};
+
+const AboutText = ({ lang } ) => {
+  if (lang === 'fr'){
+    return (<AboutFr />)
+  }
+  return (<AboutEn />)
+}
+
 const About = ({ t, i18n }) => {
   const { language } = i18n;
   return (
@@ -34,8 +73,7 @@ const About = ({ t, i18n }) => {
       <div className="row">
         <div className="col-md-12">
           <h3>{t("apropostexte")}</h3>
-
-          <AboutEn />
+          <AboutText lang={i18n.language} />
         </div>
       </div>
     </Layout>
